64c65aca5989b8929afccf5b250f0d92f38d3cf0
AlexanderB1578 <alexander.138701@gmail.com>
2026-04-17 11:47:22 +0300

    Add files via upload

---
+++ Temperatura.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{   
+    setlocale(LC_ALL, "rus");
+
+    int c;
+    int f;
+    
+    std::cout << "Введите значение температуры" << std::endl;
+    std::cin >> c;
+
+    f = c * (9/5) + 32;
+
+    std::cout << f;
+}
